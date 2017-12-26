7a02b50bf0cc84d9fc7a255c17bb3182be2bb8a8
Ivan Chaban <ivan.chaban@devico.io>
2017-12-26 15:46:58 +0200

    save and a few class

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-
 function Editor(id) {
-  this.editor = document.getElementById(id),
-  this.bold = document.getElementById('bold'),
-  this.italic = document.getElementById('italic'),
-  this.underline = document.getElementById('underline'),
-  this.fontSize = document.getElementById('fontSize'),
-  this.color = document.getElementById('color'),
-  this.get = document.getElementById('get');
-  this.clickEvents();
-  this.changeEvents();
-  this.shiftEnter();
-  this.text = document.getElementById('text')
-  this.formatText = document.getElementById('formatText')
+  this.editor = document.getElementById(id);
+  this.saveBtn = document.getElementById('save');
+  this.html = this.editor.innerHTML || '';
+  this.text = this.editor.innerText || '';
+
+  this.textField = document.getElementById('text');
 }
 
 Editor.prototype.setCommand = function (aCommandName, aValueArgument, aShowDefaultUI) {
@@ -20,30 +13,62 @@ Editor.prototype.setCommand = function (aCommandName, aValueArgument, aShowDefau
   document.execCommand(aCommandName, aShowDefaultUI, aValueArgument);
 }
 
-Editor.prototype.reset = function () {
-  fontSize.value = 0;
-  color.value = '#000000';
+Editor.prototype.getHtml = function () {
+  return this.editor.innerHTML
 }
 
-Editor.prototype.clickEvents = function () {
+Editor.prototype.result = function () {
+  this.html = this.getHtml();
+  this.textField.innerText = this.html;
+}
+
+Editor.prototype.download = function () {
+  this.saveBtn.innerHTML = '<a href="data:text/plain;charset=utf-8,%EF%BB%BF' + encodeURIComponent(this.html) + '" download="text.html">text.html</a>';
+}  
+
+Editor.prototype.getText = function () {
+  return this.editor.innerText
+}
+
+function Options(id) {
+  Editor.call(this, id);
+  
+  this.clickEvents();
+  this.changeEvents();
+
+  this.bold = document.getElementById('bold'),
+  this.italic = document.getElementById('italic'),
+  this.underline = document.getElementById('underline'),
+  this.heading = document.getElementById('heading');
+
+  this.get = document.getElementById('get');
+}
+
+Options.prototype = Object.create(Editor.prototype);
+Options.prototype.constructor = Options;
+
+
+Options.prototype.reset = function () {
+  this.heading.value = 0;
+}
+
+Options.prototype.clickEvents = function () {
   var that = this;
 
   document.addEventListener('click', function (e) {
     var command = e.target.dataset.command;
     var elem = e.target.dataset.elem;
-    if (e.target.classList.contains('heading')) {
-      that.setCommand(command, '<' + elem + '>');
-    } else if (e.target.classList.contains('btn')) {
+    if (e.target.classList.contains('btn')) {
       that.setCommand(command)
     } else if (e.target.classList.contains('getData')) {
-      console.log(1)
-      that.text.innerHTML = that.getText();
-      that.formatText.innerHTML = that.getHtml();
+      console.log(that.textField)
+      that.textField.innerText = that.getHtml();
+      that.result();
     }
-   })
+  })
 }
 
-Editor.prototype.changeEvents = function () {
+Options.prototype.changeEvents = function () {
   var that = this;
 
   document.addEventListener('change', function (e) {
@@ -53,47 +78,43 @@ Editor.prototype.changeEvents = function () {
     if (e.target.classList.contains('select')) {
       that.setCommand(command, e.target.value);
       console.log(e.target.value)
-    } else if (e.target.classList.contains('input')) {
-      that.setCommand(command)
-    } 
-  })
-}
-
-Editor.prototype.changeEvents = function () {
-  var that = this;
-
-  document.addEventListener('change', function (e) {
-    var command = e.target.dataset.command;
-    var elem = e.target.dataset.elem;
-
-    if (e.target.classList.contains('select')) { // FontSize
-      that.setCommand(command, e.target.value);
-    } else if (e.target.classList.contains('input')) {
-      that.setCommand(command, e.target.value)
-    } 
+    }
 
     that.reset();
   })
 }
 
-Editor.prototype.shiftEnter = function () {
+function Text(id) {
+  Editor.call(this, id);
+  this.enter();
+  this.input();
+}
+
+Text.prototype = Object.create(Editor.prototype);
+Text.prototype.constructor = Text;
+
+Text.prototype.enter = function () {
   var that = this;
 
   document.addEventListener('keydown', function (e) {
-  e = e || window.event;
-    if (e.shiftKey && e.keyCode == 13) {
-      that.execCommand('formatBlock', false, '<p>'); 
+    if (e.keyCode == 13) {
+      that.setCommand('formatBlock', '<p>');
     }
   })
 }
 
-Editor.prototype.getHtml = function () {
-  return this.editor.innerHTML
+Text.prototype.input = function () {
+  var that = this;
+
+  document.addEventListener('input', function (e) {
+    console.log(that.html)
+    // that.
+    that.result();
+    that.download();
+  })
 }
 
-Editor.prototype.getText = function () {
-  return this.editor.innerText
-}
 
 editor = new Editor('editor')
-
+option = new Options('editor')
+text = new Text('editor')
